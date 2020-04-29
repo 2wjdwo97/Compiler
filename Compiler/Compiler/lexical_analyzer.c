0af0adae6c073676a6c05eff693118904168774d
Lee JJ <2wjdwo97@naver.com>
2020-04-29 19:15:48 +0900

    base file

---
+++ Compiler/Compiler/lexical_analyzer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct {
+	int row;
+	int col;
+	int value;
+} element;
+
+
+
+void main() {
+	// file inupt
+	// lex()
+	// 
+}
+
+//lex(){}
+
+//isKeyword(){}
+//isType(){}
+//isID(){}
+//isInteger(){}
+//isFloatingPoint(){}
+
+//binary search(){}
